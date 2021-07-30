ceacf060d29ae04b161385c77955981a5d5cc92c
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-07-30 22:46:05 +0200

    Connected Highlights to Redux

--- src/components/WeatherDetails/Highlights.jsx
+++ src/components/WeatherDetails/Highlights.jsx
@@ -1,21 +1,41 @@
 import ProgressBar from './ProgressBar';
 import WindDirection from './WindDirection';
 import HighlightsCard from './HighlightsCard';
+import { connect } from 'react-redux';
 
-const Highlights = () => (
+const Highlights = ({ today }) => (
   <div>
     <p className="text-xl font-bold text-gray-200 mb-8">Today's Highlights</p>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <HighlightsCard name="Wind status" value="7" unit="mph">
-        <WindDirection compass="WSW" degrees={210} />
-      </HighlightsCard>
-      <HighlightsCard name="Humidity" value="84" unit="%">
-        <ProgressBar value=".1" />
-      </HighlightsCard>
-      <HighlightsCard name="Visibility" value="6,4" unit=" miles" />
-      <HighlightsCard name="Air pressure" value="998" unit=" mb" />
-    </div>
+    {today && (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <HighlightsCard
+          name="Wind status"
+          value={Math.round(today.wind_speed)}
+          unit="mph"
+        >
+          <WindDirection
+            compass={today.wind_direction_compass}
+            degrees={today.wind_direction}
+          />
+        </HighlightsCard>
+        <HighlightsCard name="Humidity" value={today.humidity} unit="%">
+          <ProgressBar value={today.humidity / 100} />
+        </HighlightsCard>
+        <HighlightsCard
+          name="Visibility"
+          value={today.visibility.toFixed(1)}
+          unit=" miles"
+        />
+        <HighlightsCard
+          name="Air pressure"
+          value={Math.round(today.air_pressure)}
+          unit=" mb"
+        />
+      </div>
+    )}
   </div>
 );
 
-export default Highlights;
+const mapStateToProps = ({ weather }) => ({ today: weather.today });
+
+export default connect(mapStateToProps)(Highlights);
